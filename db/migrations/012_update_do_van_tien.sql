d1d86fd2fc576921f7cb223e89389e67b9084255
Will Do <vquand@gmail.com>
2026-09-13 17:17:13 +0700

    fix: update do van tien birth and death years

---
+++ db/migrations/012_update_do_van_tien.sql
@@ -0,0 +1,8 @@
+UPDATE members
+SET birth_year = 1916,
+    life_status = 'deceased',
+    death_year = 2007,
+    death_date = NULL,
+    age_at_death = NULL,
+    age_at_death_qualifier = NULL
+WHERE full_name = 'Đỗ Văn Tiền';
